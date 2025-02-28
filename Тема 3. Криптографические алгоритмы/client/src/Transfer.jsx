8661712549f460df16076f6b9f7ac9d0fa816844
StarsFaLL7 <starsfall-ru@yandex.ru>
2025-02-28 15:52:16 +0500

    Laboratory work on topic 3 is completed.

---
+++ Тема 3. Криптографические алгоритмы/client/src/Transfer.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import server from "./server";
+import { keccak256 } from "ethereum-cryptography/keccak.js"
+import * as secp from "ethereum-cryptography/secp256k1";
+import { toHex } from "ethereum-cryptography/utils"
+
+function Transfer({ address, setBalance, privateKey, setPrivateKey }) {
+  const [sendAmount, setSendAmount] = useState("");
+  const [recipient, setRecipient] = useState("");
+
+  const setValue = (setter) => (evt) => setter(evt.target.value);
+
+  async function transfer(evt) {
+    evt.preventDefault();
+    const transaction = {
+      sender: address,
+      amount: parseInt(sendAmount),
+      recipient: recipient,
+      timestamp: Date.now(),
+      hash: "",
+      hexSign: ""
+    }
+    const trxHashBytes = keccak256(Uint8Array.from(JSON.stringify(transaction)));
+    transaction.hash = toHex(trxHashBytes);
+    const signature = secp.secp256k1.sign(trxHashBytes, privateKey);
+    transaction.hexSign = signature.toCompactHex();
+    console.log("trx: ", transaction);
+    try {
+      const {
+        data: { balance },
+      } = await server.post(`send`, transaction);
+      setBalance(balance);
+    } catch (ex) {
+      alert(ex.response.data.message);
+    }
+  }
+
+  return (
+    <form className="container transfer" onSubmit={transfer}>
+      <h1>Send Transaction</h1>
+
+      <label>
+        Send Amount
+        <input
+          placeholder="1, 2, 3..."
+          value={sendAmount}
+          onChange={setValue(setSendAmount)}
+        ></input>
+      </label>
+
+      <label>
+        Recipient
+        <input
+          placeholder="Type an address, for example: 0x2"
+          value={recipient}
+          onChange={setValue(setRecipient)}
+        ></input>
+      </label>
+
+      <input type="submit" className="button" value="Transfer" />
+    </form>
+  );
+}
+
+export default Transfer;
---
+++ Тема 3. Криптографические алгоритмы/server/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 3042;
+const keccak256 = require("ethereum-cryptography/keccak.js").keccak256;
+const secp256k1 = require("ethereum-cryptography/secp256k1").secp256k1;
+
+app.use(cors());
+app.use(express.json());
+
+const balances = {
+  "02ee954e295758a4fbfb4b7c5720d011bd853dc6d794ddce3ac5e4ee15b8d416d6": 100,
+  "038b91a637ec4d5dd43928c10681ae583bbb430c8e9eaa0c9c8ad004aade964b95": 50,
+  "03e905e1da384c287b496d7b81a919511db37e3957b0452295549d61052d41a553": 75,
+};
+
+app.get("/balance/:address", (req, res) => {
+  const { address } = req.params;
+  const balance = balances[address] || 0;
+  res.send({ balance });
+});
+
+app.post("/send", (req, res) => {
+  const transaction = req.body;
+  console.log(transaction);
+  const { sender, recipient, amount, hash, hexSign } = transaction;
+  const isSigned = secp256k1.verify(hexSign, hash, sender);
+  console.log("Is signed: ", isSigned);
+  if (isSigned){
+    setInitialBalance(sender);
+    setInitialBalance(recipient);
+  
+    if (balances[sender] < amount) {
+      res.status(400).send({ message: "Not enough funds!" });
+    } else {
+      balances[sender] -= amount;
+      balances[recipient] += amount;
+      res.send({ balance: balances[sender] });
+    }
+  }
+  else {
+    res.status(400).send({ message: "Not signed!" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
+
+function setInitialBalance(address) {
+  if (!balances[address]) {
+    balances[address] = 0;
+  }
+}
